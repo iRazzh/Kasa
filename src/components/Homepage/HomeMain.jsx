5e10340bd271496797579e6fb39ceae5c4eab8fa
iRazzh <tanguygerardin60@hotmail.com>
2022-09-19 19:08:17 +0200

    add data

--- src/components/Homepage/HomeMain.jsx
+++ src/components/Homepage/HomeMain.jsx
@@ -1,7 +1,7 @@
 import HomeBanner from './HomeBanner'
 import HomeCard from './HomeCard'
 import '../../styles/Homepage/HomeMain.css'
-import { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { Link } from "react-router-dom"
 
@@ -11,6 +11,49 @@ import { Link } from "react-router-dom"
 // useState 
 // Faire redescendre l'info 
 
+export function FetchData() {
+    const [ logements , setLogements ] = useState([]);
+    const getData = () => {
+      fetch('logements.json', { 
+        headers : { 
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+         }
+      })
+        .then(function(response){
+            return response.json();
+        })
+        .then(function(datas) {
+          console.log(datas);
+          setLogements(datas)
+        });
+    }
+    useEffect(() => {
+      getData()
+    }, [])
+    return (
+        <div className="App">
+         {
+           logements && logements.length > 0 && logements.map((logement)=>
+            <ul key={logement.id}>
+                <li>{logement.id} 
+                <br /> {logement.title} 
+                <br />{logement.cover} 
+                <br /> {logement.pictures}
+                <br /> {logement.description}
+                <br /> {logement.host.name}
+                <br /> {logement.host.picture}
+                <br /> {logement.rating}
+                <br /> {logement.location}
+                <br /> {logement.equipements}
+                <br /> {logement.tags}
+                </li>
+            </ul>
+           )
+         }
+        </div>
+      );
+    }
 
 export default function HomeMain() {
     return (
@@ -18,7 +61,7 @@ export default function HomeMain() {
             <HomeBanner />
             <Link to="/404"><p>404 layout</p></Link>
             <HomeCard />
-            <Fetch />
+            <FetchData />
         </main>
     );
 }
